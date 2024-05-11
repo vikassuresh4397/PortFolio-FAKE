561e1c69d6b85060182337f1da67c01cbbb5826f
vikassuresh4397 <vikassuresh4397@gmail.com>
2024-05-11 15:35:28 +0530

    portfolio2

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -372,9 +372,9 @@ const Intro = () => {
         </Link>
         {/* social icons */}
         <div className="i-icons">
-          <img src={Github} alt="" />
+          {/* <img src={Github} alt="" />
           <img src={LinkedIn} alt="" />
-          <img src={Instagram} alt="" />
+          <img src={Instagram} alt="" /> */}
         </div>
       </div>
 
